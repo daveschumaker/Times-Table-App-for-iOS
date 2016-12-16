ac516aff3e9e97d5aaa5cca93d6496de6b682678
Dave Schumaker <daveschumaker@gmail.com>
2016-12-15 21:43:59 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Times Table Demo App for iOS	
+
+App built in Xcode 8 and Swift for a Udemy online course about iOS development.
--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -8,7 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var displayValue: UILabel!
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderChange(_ sender: Any) {
+        let getInt = Int(slider.value)
+        displayValue.text = String(getInt)
+        table.reloadData()
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        let generateString = "\(indexPath.row) x \(Int(slider.value)) = \(indexPath.row * Int(slider.value))"
+        
+        cell.textLabel?.text = generateString
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
